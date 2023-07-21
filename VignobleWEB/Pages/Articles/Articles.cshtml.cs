a4a8519450ded0f7b7875a5f50ab34cf5b05b259
camille <camille.melkebeek@viacesi.fr>
2023-07-21 15:17:18 +0200

    feat : add page and front-end

---
+++ VignobleWEB/Pages/Articles/Articles.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VignobleWEB.Pages
+{
+    public class ArticlesModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public ArticlesModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
