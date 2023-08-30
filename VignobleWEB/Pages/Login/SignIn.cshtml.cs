80c0e1694d885499702d1d5823335a7338f2ce54
camille <camille.melkebeek@viacesi.fr>
2023-08-30 13:17:06 +0200

    Add page (front)

---
+++ VignobleWEB/Pages/Login/SignIn.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VignobleWEB.Pages.Login
+{
+    public class SignInModel : PageModel
+    {
+
+        public SignInModel()
+        {
+        }
+
+        public IActionResult OnGet()
+        {
+            IActionResult result = Page();
+
+            return result;
+        }
+    }
+}
---
+++ VignobleWEB/Pages/Login/SignUp.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VignobleWEB.Pages.Login
+{
+    public class SignUpModel : PageModel
+    {
+
+        public SignUpModel()
+        {
+        }
+
+        public IActionResult OnGet()
+        {
+            IActionResult result = Page();
+
+            return result;
+        }
+    }
+}
